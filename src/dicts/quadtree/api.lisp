5db0ad9b2243e590c996f8cbf98a86c68a38f5e5
Jacek Podkanski <ruby.object@googlemail.com>
2017-05-04 16:13:36 +0100

    thinking

--- src/dicts/quadtree/api.lisp
+++ src/dicts/quadtree/api.lisp
@@ -9,10 +9,42 @@
                                cl-ds:mutable)
   ())
 
-(defmethod cl-ds:at ((container quadtree) location))
-(defmethod (setf cl-ds:at) (new-value (container mutable-quadtree) location))
+(defun unimplemented () (error "not implemented"))
 
-(defmethod cl-ds:insert ((container functional-quadtree) location new-value))
-(defmethod cl-ds:add ((container functional-quadtree) location new-value))
-(defmethod cl-ds:update ((container functional-quadtree) location new-value))
-(defmethod cl-ds:erase ((container functional-quadtree) location))
+(defun make-functional-quad-tree ()
+  (unimplemented))
+
+(defun make-mutable-quad-tree ()
+  (unimplemented))
+
+;; at -> return item at coordinates (coordinates should be t, for tests array of size = 2 will be ok)
+;; insert -> insert item at coordinates, replace if exists
+;; add -> insert item at coordinates if place is not occupied
+;; update -> replace item at coordinates, do nothing otherwise
+;; erase -> you know the drill
+
+
+;; | cl-containers | cl-data-structures |
+;; | find-item     | at                 |
+;; | insert-item   | insert             |
+;; |               | add                |
+;; |               | update             |
+;; | empty!        | erase              |
+
+(defmethod cl-ds:at ((container functional-quadtree) location)
+  (unimplemented))
+
+(defmethod (setf cl-ds:at) (new-value (container mutable-quadtree) location)
+  (unimplemented))
+
+(defmethod cl-ds:insert ((container functional-quadtree) location new-value)
+  (unimplemented))
+
+(defmethod cl-ds:add ((container functional-quadtree) location new-value)
+  (unimplemented))
+
+(defmethod cl-ds:update ((container functional-quadtree) location new-value)
+  (unimplemented))
+
+(defmethod cl-ds:erase ((container functional-quadtree) location)
+  (unimplemented))
